67d0e14072b2fc7c8414f66aab8032335252af93
pmy09 <69659683+pmy09@users.noreply.github.com>
2020-10-09 19:25:19 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,15 +1,7 @@
 $(document).ready(function() {
-  $('#select-menu').on('change', function() {
-    //console.log('Hello');
-    let name= $('#select-menu option:selected').text();
-    let distance= $('#select-menu option:selected').val();
-    let price= $('#select-menu option:selected').data('price');
-    console.log(price);
-    if (distance) {
-      $('#feedback-message').text('You are signing up for a '+name + ', which costs ' + price + ', to a distance of ' + distance + 'km');
-    }
-    else {
-      $('#feedback-message').empty();
-    }
+  $('#input-name').on('keyup', function(){
+    console.log('Press!');
+    let name= $(this).val();
+    $('#feedback-message').text('Pleased to meet you, '+ name);
   })
 });
