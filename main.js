1bdaf5608c48ee1debf0c6443cb2e437b01975d4
pmy09 <69659683+pmy09@users.noreply.github.com>
2020-10-09 19:30:09 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,11 +1,3 @@
 $(document).ready(function() {
-  $('#input-name').on('keyup', function(){
-    console.log('Press!');
-    let name= $(this).val();
-    $('#feedback-message').text('Pleased to meet you, '+ name);
-  })
-  $('a').on('click', function(event){
-    event.preventDefault();
-    $('#feedback-message').text("That's fine");
-  })
+  
 });
