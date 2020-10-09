7ae48363586f22c59b52969538371f9399c152ac
pmy09 <69659683+pmy09@users.noreply.github.com>
2020-10-09 19:27:13 +0100

    Update main.js

--- main.js
+++ main.js
@@ -4,4 +4,8 @@ $(document).ready(function() {
     let name= $(this).val();
     $('#feedback-message').text('Pleased to meet you, '+ name);
   })
+  $('a').on('click', function(event){
+    event.preventDefault();
+    $('#feedback-message').text("That's fine");
+  })
 });
