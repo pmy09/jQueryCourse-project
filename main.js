121de6585feab7ff15775ebb1752a4b0e0fb1195
pmy09 <69659683+pmy09@users.noreply.github.com>
2020-10-09 19:18:49 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,8 +1,15 @@
 $(document).ready(function() {
-  $('.box').on('click', '.box-button', function() {
-    //console.log('Hello')
-    $(this).parent().toggleClass('highlight');
-  });
-
-
+  $('#select-menu').on('change', function() {
+    //console.log('Hello');
+    let name= $('#select-menu option:selected').text();
+    let distance= $('#select-menu option:selected').val();
+    let price= $('#select-menu option:selected').data('price');
+    console.log(price);
+    if (distance) {
+      $('#feedback-message').text('You are signing up for a '+name + ', which costs ' + price + ', to a distance of ' + distance + 'km');
+    }
+    else {
+      $('#feedback-message').empty();
+    }
+  })
 });
