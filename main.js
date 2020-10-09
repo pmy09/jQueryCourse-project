c636e64c0688ea71586059c367c6e8a6714ae8d6
pmy09 <69659683+pmy09@users.noreply.github.com>
2020-10-09 19:00:41 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
-console.log('Hello World!')
 $(document).ready(function() {
-  console.log($(document));
-  $('body').html('<strong>Hello World!</strong>');
+  $('#fire').addClass('highlight');
+
 
 });
