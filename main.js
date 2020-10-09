cc441305818942717c1c167d6331ea9efb30e786
pmy09 <69659683+pmy09@users.noreply.github.com>
2020-10-09 20:07:41 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,25 +1,22 @@
+function addItem(name, description, price, moreInfo){
+  let html= '';
+  html += '<div class="item">';
+  html += '<div class="name">'+ name + '</div>';
+  html += '<img src="assets/beach.jpg"></img>';
+  html += '<div class="description">'+ description +'</div>';
+  html += '<div class="price">'+ price +'</div>';
+  html += '<button class="item-add">Add to cart</button>';
+  html += '<button class="item-remove">Remove</button>';
+  html += '<br/>';
+  html += '<a class="more-info-link" href="#">More info</a>';
+  html += '<div class="more-info">'+ moreInfo +'</div>';
+  html += '</div>';
+
+  $('#container').prepend(html);
+}
+
+
 $(document).ready(function() {
-  $("#button-create-item").on('click', function(){
-    let name= $('input-create-item').val();
-    //console.log(name);
-    $('#input-create-item').val('');
-
-    let html= '';
-    html += '<div class="item">';
-    html += '<div class="name">'+ name + '</div>';
-    html += '<img src="assets/beach.jpg"></img>';
-    html += '<div class="description">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</div>';
-    html += '<div class="price">499</div>';
-    html += '<button class="item-add">Add to cart</button>';
-    html += '<button class="item-remove">Remove</button>';
-    html += '<br/>';
-    html += '<a class="more-info-link" href="#">More info</a>';
-    html += '<div class="more-info">Lorem ipsum dolor sit amet</div>';
-    html += '</div>';
-
-    $('#container').prepend(html)
-  });
-
   $('#container').on('click','.more-info-link', function(event){
     event.preventDefault();
     //$(this).parent().find('.more-info').toggle('fast');
@@ -33,7 +30,23 @@ $(document).ready(function() {
     $(this).parent().remove();
   });
 
-  $.ajax('data/item.json', function(response){
-    console.log(response);
+  $.ajax('data/item.json',{
+    dataType: 'json',
+    contentType: 'application/json',
+    cache: false
+  }).done(function(response){
+    //console.log(response);
+    let items = response.items;
+    //console.log(items);
+    items.forEach(function(item){
+      console.log(item);
+      addItem(item.name, item.description, item.price, item.moreInfo);
+
+    });
+
+  }).fail(function(request, errorType, errorMessage){
+    console.log(errorMessage);
+  }).always(function(){
+
   })
 });
