a63f9e782f30b746d64f43cf88280c6e1cc1c522
pmy09 <69659683+pmy09@users.noreply.github.com>
2020-10-09 19:38:16 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,21 @@
 $(document).ready(function() {
-  
+  $("#button-create-item").on('click', function(){
+    let name= $('input-create-item').val();
+    //console.log(name);
+    $('#input-create-item').val('');
+
+    let html= '';
+    html += '<div class="item">';
+    html += '<div class="name">'+ name + '</div>';
+    html += '<img src="assets/beach.jpg"></img>';
+    html += '<div class="description">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</div>';
+    html += '<div class="price">499</div>';
+    html += '<button>Add to cart</button>';
+    html += '<br/>';
+    html += '<a href="#">More info</a>';
+    html += '<div class="more-info">Lorem ipsum dolor sit amet</div>';
+    html += '</div>';
+
+    $('#container').prepend(html)
+  })
 });
