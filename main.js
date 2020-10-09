542f786d5942cbc2cb3d1805e9ccea99b123ded4
pmy09 <69659683+pmy09@users.noreply.github.com>
2020-10-09 19:41:03 +0100

    Update main.js

--- main.js
+++ main.js
@@ -10,12 +10,17 @@ $(document).ready(function() {
     html += '<img src="assets/beach.jpg"></img>';
     html += '<div class="description">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</div>';
     html += '<div class="price">499</div>';
-    html += '<button>Add to cart</button>';
+    html += '<button class="item-add">Add to cart</button>';
+    html += '<button class="item-remove">Remove</button>';
     html += '<br/>';
     html += '<a href="#">More info</a>';
     html += '<div class="more-info">Lorem ipsum dolor sit amet</div>';
     html += '</div>';
 
     $('#container').prepend(html)
-  })
+  });
+  $('#container').on('click','.item-remove', function(){
+    //console.log('Hello');
+    $(this).parent().remove();
+  });
 });
